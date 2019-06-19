ce84f3432838ab571edfb1a5929b9711ec8b0943
test-git <dholariyarahul@gmail.com>
2019-06-19 10:43:18 +0530

    Update Templates folder

--- application.py
+++ application.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "hello World!!!!"
+    return render_template("index.html")
+
 
 @app.route("/<string:name>")
 def david(name):
